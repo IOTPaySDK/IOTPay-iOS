18a2c239803972345713cf141dc74da723583b28
Dan-YY <dan@dansstudio.ca>
2021-04-25 19:39:28 -0700

    Initial Commit

---
+++ IOTPayiOS/IOTPayiOS.h
@@ -0,0 +1,18 @@
+//
+//  IOTPayiOS.h
+//  IOTPayiOS
+//
+//  Created by macbook on 2021-04-25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOTPayiOS.
+FOUNDATION_EXPORT double IOTPayiOSVersionNumber;
+
+//! Project version string for IOTPayiOS.
+FOUNDATION_EXPORT const unsigned char IOTPayiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOTPayiOS/PublicHeader.h>
+
+
