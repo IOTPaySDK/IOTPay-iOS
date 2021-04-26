9cd5d4ff03aef6e425f3d0523a5752a4489292aa
Dan-YY <dan@dansstudio.ca>
2021-04-25 19:46:19 -0700

    examples init

---
+++ Examples/AddCardObjcExample/AddCardObjcExample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AddCardObjcExample
+//
+//  Created by macbook on 2021-04-25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-IOTPayiOS-IOTPayiOSTests/Pods-IOTPayiOS-IOTPayiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOTPayiOS_IOTPayiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_IOTPayiOS_IOTPayiOSTests
+@end
