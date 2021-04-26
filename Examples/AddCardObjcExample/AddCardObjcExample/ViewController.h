af61357dfa8f4cc0eda67480f487f6eca46b311f
Dan-YY <dan@dansstudio.ca>
2021-04-25 19:24:26 -0700

    4 example init

---
+++ Examples/AddCardObjcExample/AddCardObjcExample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AddCardObjcExample
+//
+//  Created by macbook on 2021-04-25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Examples/OneTimePurchaseObjcExample/OneTimePurchaseObjcExample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  OneTimePurchaseObjcExample
+//
+//  Created by macbook on 2021-04-25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
